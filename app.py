1fcb9f44b3d864ea54ee21673a3689a8ed1f2e0a
tukky-14 <bigoonage0104@gmail.com>
2023-03-13 20:20:34 +0900

    環境変数からキーを取得するよう修正

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 # https://chatgpt-lab.com/n/nda0de0be1774
+import os
 import openai
 
-openai.api_key = 'OpenAI API シークレットキー'
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[
-                    {'role': 'system', 'content': 'You are a helpful assisant.'},
-                    {'role': 'user', 'content': '開発に便利な、みんなが知らない知識を1つ教えてください。'}
-                ],
-                temperature=0.0,
+    model="gpt-3.5-turbo",
+    messages=[
+        {'role': 'system', 'content': 'あなたは優秀なITエンジニアです。'},
+        {'role': 'user', 'content': 'どのプログラミング言語でも役に立つ、意外と知られていないITエンジニアの豆知識を1つ教えてください。'}
+    ],
+    temperature=0.0,
 )
 res_content = response['choices'][0]['message']['content'] 
 print(res_content)
