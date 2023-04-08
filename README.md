6f9cff3954cb5f6d64b6d87c21740efa2aee8249
tukky-14 <bigoonage0104@gmail.com>
2023-04-08 23:10:22 +0900

    ChatGPTを使用し、孫悟空の人格でLINEに返信するよう更新

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-- line-bot-sdk のインストール
+- モジュールのインストール
+
   ```
+  # line-bot-sdk
   pip install line-bot-sdk -t .
+
+  # openai
+  pip install open -t .
   ```
--- lambda_function.py
+++ lambda_function.py
@@ -1,25 +1,33 @@
 import os
 import json
+import openai
+import personality
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
-# 環境変数からLINE Botのチャネルアクセストークンを取得
 LINE_CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
-# チャネルアクセストークンを使用して、LineBotApiのインスタンスを作成
 LINE_BOT_API = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 def lambda_handler(event, context):
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
     try:
-        # LINEからメッセージを受信
         if event['events'][0]['type'] == 'message':
-        # メッセージタイプがテキストの場合
             if event['events'][0]['message']['type'] == 'text':
             # リプライ用トークン
                 replyToken = event['events'][0]['replyToken']
                 # 受信メッセージ
                 messageText = event['events'][0]['message']['text']
-                # メッセージを返信（受信メッセージをそのまま返す）
-                LINE_BOT_API.reply_message(replyToken, TextSendMessage(text=messageText))
+                # ChatGPT-3.5-turboで返信
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {'role': 'system', 'content': personality.SONGOKU},
+                        {'role': 'user', 'content': messageText }
+                    ],
+                    temperature=0.0,
+                )
+                res_content = response['choices'][0]['message']['content']
+                LINE_BOT_API.reply_message(replyToken, TextSendMessage(text=res_content))
 
     # エラーが起きた場合
     except Exception as e:
